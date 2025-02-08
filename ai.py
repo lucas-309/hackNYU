372e595629f89b8488308e414eb80cab4bcd9b53
ecc2205@columbia.edu <ecc2205@columbia.edu>
2025-02-08 12:52:35 -0500

    ai integration

---
+++ ai.py
@@ -0,0 +1,6 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field 
+from typing import List, Dict 
+import os 
+from dotenv import load_dotenv 
